0e13ac9585c8ebbeff86347d2a1abd7df5ab8ae0
Sergey Parshukov <sergey.parshukov@me.com>
2014-12-10 15:50:13 +0300

    Permissions check

---
+++ DoorLock/DoorLock-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  DoorLock-Bridging-Header.h
+//  DoorLock
+//
+//  Created by Sergey Parshukov on 31.10.14.
+//  Copyright (c) 2014 Interactive Lab. All rights reserved.
+//
+
+#ifndef DoorLock_DoorLock_Bridging_Header_h
+#define DoorLock_DoorLock_Bridging_Header_h
+
+// #import "..."
+
+#endif
--- DoorLock/ViewController.swift
+++ DoorLock/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var beakonAvailableLabel: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
+    func initialize() {
+        locationManager.delegate = self
+        
+        let beaconsAvailable = CLLocationManager.isMonitoringAvailableForClass(CLBeaconRegion)
+        beakonAvailableLabel?.text = "Beacons: " + (beaconsAvailable ? "available" : "not available")
+ 
+        if(beaconsAvailable) {
+            if(isAuthorized(CLLocationManager.authorizationStatus())) {
+                initRegions()
+            } else {
+                locationManager.requestAlwaysAuthorization()
+            }
+        }
+        
+        let backgroundAvailable = UIApplication.sharedApplication().backgroundRefreshStatus == UIBackgroundRefreshStatus.Available
+        label2?.text = "Background refresh: " + (backgroundAvailable ? "OK" : "Disabled")
+    }
+    
+    func initRegions() {
+    }
+    
+    func isAuthorized(status: CLAuthorizationStatus) -> Bool {
+        let result = status == CLAuthorizationStatus.Authorized
+        label3?.text = "Access: " + (result ? "authorized" : "not authorized")
+        return result
+    }
+
+
+    
+    // MARK: iOS API
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if isAuthorized(status) {
+            initRegions()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initialize()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +64,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
